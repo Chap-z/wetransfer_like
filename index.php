533aab0f6caa0d0fce24dc720721e09ff2af7dc3
Martint033 <martint033@gmail.com>
2018-05-28 15:44:24 +0200

    routeur

--- index.php
+++ index.php
@@ -4,12 +4,22 @@
 require('models/compress.class.php');
 require('models/mail.class.php');
 
+$url = explode("/",$_SERVER['REQUEST_URI'],4);
+if (count($url)>3){
+    array_pop($url);
+}
+$path = implode("/", $url);
+
+switch($path){
+    case "/meme-generator":
+    case "/meme-generator/" :
+        require('controllers/ctrlHomepage.php');
+        break;
+}
+
 
 $truc = new CompressClass();
-
 $truc-> compress('mcd.jpg');
-
 $mail = new MailSend();
-
 $mail-> sendMail();
 
