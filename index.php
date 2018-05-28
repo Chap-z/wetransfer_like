5f1b29b373cd36383adf71a8cb7f3d4021f1a782
Chap-z <cha.lacroix@hotmail.Fr>
2018-05-28 16:02:49 +0200

    twig histoire de voir ce qu'on fait

--- index.php
+++ index.php
@@ -4,22 +4,44 @@
 require('models/compress.class.php');
 require('models/mail.class.php');
 
-$url = explode("/",$_SERVER['REQUEST_URI'],4);
-if (count($url)>3){
-    array_pop($url);
-}
-$path = implode("/", $url);
+// $url = explode("/",$_SERVER['REQUEST_URI'],4);
+// if (count($url)>3){
+//     array_pop($url);
+// }
+// $path = implode("/", $url);
 
-switch($path){
-    case "/meme-generator":
-    case "/meme-generator/" :
-        require('controllers/ctrlHomepage.php');
+// switch($path){
+    
+//     case "/home" :
+//         require('controllers/ctrl_home.php');
+//         break;
+// }
+
+
+if (isset($_GET['action'])){
+     
+    switch ($_GET['action']) { 
+        
+    //routeur
+        //case 'affiche':
+        //require_once('controleur/affiche.php');
+        //break;
+        case 'home':
+        require_once('controllers/ctrl_home.php');
+        // require_once('controllers/control_generator.php');
+            echo 'coucou le grand';
         break;
+        default:
+        require_once('error.html');
+    
+    }
+} else {
+    require_once('controllers/ctrl_home.php');
+
 }
 
-
-$truc = new CompressClass();
-$truc-> compress('mcd.jpg');
-$mail = new MailSend();
-$mail-> sendMail();
+// $truc = new CompressClass();
+// $truc-> compress('mcd.jpg');
+// $mail = new MailSend();
+// $mail-> sendMail();
 
