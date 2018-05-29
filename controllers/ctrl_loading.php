ad61001816cb20ec28588b15d1c8f895bb840b2e
Chap-z <cha.lacroix@hotmail.Fr>
2018-05-29 10:56:12 +0200

    upload

--- controllers/ctrl_loading.php
+++ controllers/ctrl_loading.php
@@ -1,9 +1,12 @@
 <?php
 
-$target_dir = "assets/files";
+$target_dir = "wetranfer_like_";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
-$name = $_FILES["fileToUpload"]["name"];
+$path = $_FILES["fileToUpload"]["tmp_name"];
 
-var_dump($name);
+var_dump($target_file);
+var_dump($path);
 
+$truc = new CompressClass();
+$truc-> compress($path, $target_file);
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require('models/compress.class.php');
 require('models/mail.class.php');
 
@@ -9,44 +8,37 @@ if (count($url)>3){
     array_pop($url);
 }
 $path = implode("/", $url);
- echo($path);
-switch($path){
-    
-    case "/wetransfer_like/home" :
-        require('controllers/ctrl_home.php');
-        break;
+ echo($path); ///home/Charlotte/public_html
 
-    case "/wetransfer_like/loading":
-        require("controllers/ctrl_loading.php");
-        break;
+ //OK J'AI COMPRIS ///home/Charlotte/public_html
 
-    default:
-        require_once('controllers/ctrl_home.php');
-        break;
-}
-
-
-// if (isset($_GET['action'])){
+if (isset($_GET['action'])){
      
-//     switch ($_GET['action']) { 
+    switch ($_GET['action']) { 
         
-//     //routeur
-//         //case 'affiche':
-//         //require_once('controleur/affiche.php');
-//         //break;
-//         case 'home':
-//         require_once('controllers/ctrl_home.php');
-//         // require_once('controllers/control_generator.php');
-//             echo 'coucou le grand';
-//         break;
-//         default:
-//         require_once('error.html');
-    
-//     }
-// } else {
-    // require_once('controllers/ctrl_home.php');
+    //routeur
+        //case 'affiche':
+        //require_once('controleur/affiche.php');
+        //break;
+        case 'home':
+        require_once('controllers/ctrl_home.php');
+        // require_once('controllers/control_generator.php');
+        break;
 
-// }
+        case 'loading':
+        require_once('controllers/ctrl_loading.php');
+        // require_once('controllers/control_generator.php');
+            echo 'coucou le grand';
+        break;
+
+        default:
+        require_once('error.html');
+    
+    }
+} else {
+    require_once('controllers/ctrl_home.php');
+
+}
 
 // $truc = new CompressClass();
 // $truc-> compress('mcd.jpg');
