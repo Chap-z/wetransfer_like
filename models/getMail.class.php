6ec7291001927eb007618608f7a83ee44fe926f8
Chap-z <cha.lacroix@hotmail.Fr>
2018-05-31 11:05:24 +0200

    recup mail ok

--- models/getMail.class.php
+++ models/getMail.class.php
@@ -2,21 +2,33 @@
 class GetMail{
 
 	protected $_db;
-	protected $zip; 
+
 		
-	// protected $_table;
+	public function __construct(){
 
-	// public function __construct(){
-	// 	$this->_db = new Bddmanager();
-	// 	$this->_db = $this->_db->getBdd();
-	// 	// $this->_table = 'me_link_meme_image';
-	// }
+		$this->db = new Bddmanager();
+		$this->db = $this->db->getBdd();
 
-	public function get_mail(){
-		// $req = $this->_db->prepare('INSERT INTO ' . $this->_table . ' (id_meme, id_image) VALUES (:id_meme, :id_image)');
-		// $req->bindParam(':id_meme', $idMeme);
-		// $req->bindParam(':id_image', $idImage);
-		// $req->execute();
+	}
+	
+
+	public function get_mail_send($mail){
+
+        $req = $this->db->prepare('INSERT INTO sender (mail) VALUES (:mail)');
+        $req->bindParam(':mail', $mail);
+
+        $req->execute();
+        // return $req->fetch(PDO::FETCH_ASSOC);    
+		
+	}
+
+	public function get_mail_recep($mail){
+
+        $req = $this->db->prepare('INSERT INTO recipient (mail) VALUES (:mail)');
+        $req->bindParam(':mail', $mail);
+
+        $req->execute();
+        // return $req->fetch(PDO::FETCH_ASSOC);    	
 
 	}
 }
