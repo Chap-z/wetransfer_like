9117659602826aefe7844e237f5b6f1546cd968f
Martint033 <martint033@gmail.com>
2018-05-31 16:53:01 +0200

    back OK

--- controllers/ctrl_download.php
+++ controllers/ctrl_download.php
@@ -1,4 +1,14 @@
 <?php
 
-$fileUpload = $_GET['action'];
-echo($fileUpload);
+require_once ('vendor/autoload.php');
+$loader = new Twig_Loader_Filesystem('views');
+ $twig = new Twig_Environment($loader,[
+     'cache' => false,
+ ]); 
+
+
+$fileUpload = 'assets/files/'.$_GET['name'].'.zip';
+
+
+$template = $twig->load('download.html');
+echo $template->render(array("url" => $fileUpload)); 
