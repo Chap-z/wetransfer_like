41253cada4d9fc2bf427739d21f8cdfa835366ca
Chap-z <cha.lacroix@hotmail.Fr>
2018-06-01 15:46:24 +0200

    reparation scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,10 +57,8 @@
 
 
 $(document).ready(function(){ 
-    (function(){ 
         var i = 0; 
         setInterval(function(){ 
             $('#background').removeClass("bg1, bg2, bg3, bg4").addClass("bg"+(i++%4 + 1)); 
         }, 10000); 
-    })(); 
 });
