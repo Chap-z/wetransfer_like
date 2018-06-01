a4b4a6d3fa547a7aa6b55c59b8641e6c6cda6293
Chap-z <cha.lacroix@hotmail.Fr>
2018-06-01 09:28:04 +0200

    recup loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,22 @@
 //   }
 
 //   $("#validate").bind("click", validate);
+
+// function checkMail(emailAdress)
+
+// {
+//     var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+
+//     return (reg.test(emailAdress));
+// }
+
+// function valider() {
+
+//     if (!checkMail(document.form.send.value) || (document.form.send.value.length == 0)) {
+//         document.getElementById('error-mail').innerHTML = "Merci de renseigner votre email.";
+//         return false;
+//     } else if (!checkMail(document.form.recep.value) || (document.form.recep.value.length == 0)) {
+//         document.getElementById('error-mail').innerHTML = "Merci de renseigner le mail du destinataire.";
+//         return false;
+//     }
+// }
